263e801f4dc4253cfcf8294b93d774b9f11a1104
Kalhara Perera <adorekasun@gmail.com>
2020-12-29 22:59:18 +0530

    Added WebSocketSpring Project BackEnd

---
+++ Spring-Angular Socket/WebSocketSpring/src/test/java/com/adore96/WebSocketSpring/WebSocketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adore96.WebSocketSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
